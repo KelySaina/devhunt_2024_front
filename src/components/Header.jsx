4b4bda95df2c13a7f4f7694b3a700f2c0e631ce5
MurielArii <murielarisoaran@gmail.com>
2024-06-14 23:01:39 +0300

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import Image from '../images/Header.png'
 const Header = () => {
     return (
         <div className="grid grid-cols-2">
-            <div className="flex flex-col justify-center space-y-5 mx-16">
+            <div className="flex flex-col justify-center space-y-8 mx-16">
                 <div className='space-y-1'>
                     <h1 className="text-4xl">Tongasoa</h1>
                     <h1 className="text-4xl">Eto amin'ny CitizenConnect</h1>
                 </div>
-                <div className="space-y-1">
+                <div className="space-y-3">
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin finibus velit metus, non tincidunt ex lacinia vel. Pellentesque tincidunt id eros et elementum. Nunc condimentum, ligula et ornare pellentesque, lectus lectus efficitur lacus, sed ultrices urna leo nec purus. Integer vel tincidunt felis. Proin vitae risus erat. Integer laoreet ante vel augue tristique</p>
                 </div>
@@ -16,7 +16,7 @@ const Header = () => {
                 </div>
             </div>
             <div className="flex justify-center items-center">
-                <img className='w-[570px]' src={Image}/>
+                <img className='w-[600px]' src={Image}/>
             </div>
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,22 +21,15 @@ const Navbar = () => {
                 <a className="btn btn-ghost text-xl">CitizenConnect</a>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
-                </ul>
+
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <ul className="menu menu-horizontal px-40">
+                    <li><a>Fandraisana</a></li>
+                    <li><a>Ireo Serivisy</a></li>
+                    <li><a>Item 3</a></li>
+                </ul>
+            
             </div>
         </div>
     )
