baa0b5e9b5808b8073f74e55ef428432425d4cc6
MurielArii <murielarisoaran@gmail.com>
2024-06-15 00:08:08 +0400

    front login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,39 @@
+import { useState } from "react"
 import LoginForm from "../components/LoginForm"
+import { Link } from "react-router-dom"
 const Login = () => {
+    const [hoverLogin, setHoverLogin] = useState('')
+    const [hoverRegister, setHoverRegister] = useState('!opacity-0')
+
+    const handleHoverLogin = () => {
+        setTimeout(() => {
+            setHoverLogin('translate-x-0')
+            setHoverRegister('!opacity-0')
+        }, 500);
+    }
+    const handleHoverRegister = () => {
+        setTimeout(() => {
+            setHoverLogin('opacity-0')
+            setHoverRegister('!translate-x-0 opacity-20')
+        }, 500);
+    }
     return (
         <div>
             <div className="grid grid-cols-2 h-screen">
-                <div className="flex justify-center items-center">
+                <div onMouseEnter={handleHoverLogin} className="flex justify-center items-center relative">
                     <LoginForm />
+                    <img src="/images/login.svg" className={`${hoverLogin} duration-700 transform ease-in-out object-cover w-1/2 absolute bottom-0 right-0 -z-50 opacity-30 translate-x-[100%]`} />
                 </div>
-                <div className="flex flex-col items-center justify-center space-y-7 bg-success">
-                    <h1 className="text-4xl text-white">Hello, Friend!</h1>
-                    <div className="space-y-1 text-white">
-                        <p>To keep connected with us please </p>
-                        <p>Login with your personal info</p>
+                <div onMouseEnter={handleHoverRegister} className="flex flex-col items-center justify-center bg-success space-y-7 relative">
+                    <h1 className="text-5xl text-white font-bold">N'as pas encore de compte?</h1>
+                    <div className="space-y-1 text-white flex justify-center">
+                        <p className="w-2/3 text-center text-2xl">
+                            Découvrez tous vos services publics! Connectez-vous dès maintenant pour simplifier vos démarches et gagner du temps
+                        </p>
                     </div>
                     <div>
-                        <button className="btn rounded-full text-success hover:scale-110 hover:bg-white">Créer compte</button>
+                        <Link to={"/register"} className="btn rounded-full text-success hover:scale-110 hover:bg-white">Créer compte</Link>
+                        <img src="/images/regsiter.svg" className={`${hoverRegister} duration-700 transform ease-in-out object-cover w-1/2 absolute top-0 left-0 -translate-x-[100%]`} />
                     </div>
                 </div>
             </div>
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,16 +1,17 @@
+import { Link } from "react-router-dom";
 import RegistrationForm from "../components/RegistrationForm";
 
 const Registration = () => {
     return (
         <div className="grid grid-cols-2 h-screen">
             <div className="flex flex-col items-center justify-center space-y-7 bg-success">
-                <h1 className="text-4xl text-white">Welcome Back!</h1>
+                <h1 className="text-6xl text-white font-bold">Completer les étapes</h1>
+                <img src="/images/step.svg" alt="step" className="w-1/2" />
                 <div className="space-y-1 text-white">
-                    <p>To keep connected with us please </p>
-                    <p>Login with your personal info</p>
+                    <p className="text-2xl">Tu as déjà un compte ?</p>
                 </div>
                 <div>
-                    <button className="btn rounded-full text-success hover:scale-110 hover:bg-white">Se connecter</button>
+                    <Link to={"/"} className="btn rounded-full text-success hover:scale-110 hover:bg-white">Se connecter</Link>
                 </div>
             </div>
             <div className="flex justify-center items-center">
