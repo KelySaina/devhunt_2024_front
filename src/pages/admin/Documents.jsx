a798ac20f13d284ff5268511c004702950fb201c
MurielArii <murielarisoaran@gmail.com>
2024-06-15 02:12:03 +0400

    init forms

---
+++ src/pages/admin/Documents.jsx
@@ -0,0 +1,11 @@
+const Documents = () => {
+    return (
+        <div className="space-y-7">
+            <div>
+                <h1 className='text-4xl first-letter:text-5xl font-bold'>List of your submissions</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Documents
--- src/services/ServicesService.js
+++ src/services/ServicesService.js
@@ -9,6 +9,23 @@ class ServicesService {
             throw new Error('Failed to get services:', error.message);
         }
     }
+
+    async submitForService(data, endpoint) {
+        try {
+            const url = `${HOST}${endpoint}`;
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+            console.log(response);
+            return response.json();
+        } catch (error) {
+            throw new Error('Failed to submit service:', error.message);
+        }
+    }
 }
 
 export default new ServicesService();
