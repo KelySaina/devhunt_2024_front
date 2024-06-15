f84d3c0c3a944e29bc20d2a186cb2f2b6980db4d
MurielArii <murielarisoaran@gmail.com>
2024-06-15 03:43:03 +0300

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,14 +25,14 @@ const Login = () => {
                     <img src="/images/login.svg" className={`${hoverLogin} duration-700 transform ease-in-out object-cover w-1/2 absolute bottom-0 right-0 -z-50 opacity-30 translate-x-[100%]`} />
                 </div>
                 <div onMouseEnter={handleHoverRegister} className="flex flex-col items-center justify-center bg-success space-y-7 relative">
-                    <h1 className="text-5xl text-white font-bold">N'as pas encore de compte?</h1>
+                    <h1 className="text-5xl text-white font-bold">Mbola tsy manana kaonty?</h1>
                     <div className="space-y-1 text-white flex justify-center">
                         <p className="w-2/3 text-center text-2xl">
-                            Découvrez tous vos services publics! Connectez-vous dès maintenant pour simplifier vos démarches et gagner du temps
+                        Fantaro ny serivisy ho an'ny daholobe rehetra! Midira izao mba hanatsorana ny fomba fanaonao ary hitsitsy fotoana
                         </p>
                     </div>
                     <div>
-                        <Link to={"/register"} className="btn rounded-full text-success hover:scale-110 hover:bg-white">Créer compte</Link>
+                        <Link to={"/register"} className="btn rounded-full text-success hover:scale-110 hover:bg-white">Hamorona kaonty</Link>
                         <img src="/images/regsiter.svg" className={`${hoverRegister} duration-700 transform ease-in-out object-cover w-1/2 absolute top-0 left-0 -translate-x-[100%]`} />
                     </div>
                 </div>
