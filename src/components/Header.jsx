1b8a5d508673e775bcbc7587d14db21ccd887999
MurielArii <murielarisoaran@gmail.com>
2024-06-15 03:25:06 +0300

    font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,6 @@
 import Image from '../images/Header.png'
 const Header = () => {
     return (
-        // <div className="grid grid-cols-2">
-        //     <div className="flex flex-col justify-center space-y-8 mx-20">
-        //         <div className='space-y-3'>
-        //             <h1 className="text-4xl">Tongasoa</h1>
-        //             <h1 className="text-4xl">Eto amin'ny </h1>
-        //             <h1 className="text-4xl">CitizenConnect</h1>
-        //         </div>
-        //         <div className="space-y-1">
-        //             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin finibus velit metus, non tincidunt ex lacinia vel. Pellentesque tincidunt id eros et elementum. Nunc condimentum, ligula et ornare pellentesque, lectus lectus efficitur lacus, sed ultrices urna leo nec purus. Integer vel tincidunt felis. Proin vitae risus erat. Integer laoreet ante vel augue tristique</p>
-        //         </div>
-        //         <div>
-        //             <button className="btn btn-success text-white rounded-full hover:scale-110">Se connecter</button>
-        //         </div>
-        //     </div>
-        //     <div className="flex justify-center items-center">
-        //         <img className='w-[500px]' src={Image} />
-        //     </div>
-        // </div>
         <div className="overflow-hidden">
             <div className="text-center text-7xl py-6">
                 <h1>Tongasoa eto amin'ny</h1>
