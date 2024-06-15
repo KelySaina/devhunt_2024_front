c1666596a4fbf5ec8719700c0b4e0ede822c68e8
MurielArii <murielarisoaran@gmail.com>
2024-06-15 06:59:59 +0400

    login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,9 +46,19 @@ const Navbar = () => {
             <div className="navbar-end">
                 {
                     isLogged ?
-                        <Link to={"/admin"} className="btn btn-success rounded-full text-white font-bold">Iditra am kaonty</Link>
+                        <Link to={"/admin"} className="btn btn-success rounded-full text-white font-bold">
+                            Mon espace
+                            <svg fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                            </svg>
+                        </Link>
                         :
-                        <Link to={"/login"} className="btn btn-success rounded-full text-white">Hisoratra anarana</Link>
+                        <Link to={"/login"} className="btn btn-success rounded-full text-white">
+                            Se connecter
+                            <svg fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                            </svg>
+                        </Link>
                 }
             </div>
         </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
 
     const handleHoverLogin = () => {
         setTimeout(() => {
-            setHoverLogin('translate-x-0')
+            setHoverLogin('!translate-x-0')
             setHoverRegister('!opacity-0')
         }, 500);
     }
@@ -25,14 +25,14 @@ const Login = () => {
                     <img src="/images/login.svg" className={`${hoverLogin} duration-700 transform ease-in-out object-cover w-1/2 absolute bottom-0 right-0 -z-50 opacity-30 translate-x-[100%]`} />
                 </div>
                 <div onMouseEnter={handleHoverRegister} className="flex flex-col items-center justify-center bg-success space-y-7 relative">
-                    <h1 className="text-5xl text-white font-bold">Mbola tsy manana kaonty?</h1>
+                    <h1 className="text-5xl text-white font-bold text-center">Ne possède pas encore de compte?</h1>
                     <div className="space-y-1 text-white flex justify-center">
                         <p className="w-2/3 text-center text-2xl">
-                        Fantaro ny serivisy ho an'ny daholobe rehetra! Midira izao mba hanatsorana ny fomba fanaonao ary hitsitsy fotoana
+                            Découvrez tous les services publics ! Connectez-vous maintenant pour simplifier vos démarches et gagner du temps.
                         </p>
                     </div>
                     <div>
-                        <Link to={"/register"} className="btn rounded-full text-success hover:scale-110 hover:bg-white">Hamorona kaonty</Link>
+                        <Link to={"/register"} className="btn rounded-full text-success hover:scale-110 hover:bg-white">Creer un compte</Link>
                         <img src="/images/regsiter.svg" className={`${hoverRegister} duration-700 transform ease-in-out object-cover w-1/2 absolute top-0 left-0 -translate-x-[100%]`} />
                     </div>
                 </div>
