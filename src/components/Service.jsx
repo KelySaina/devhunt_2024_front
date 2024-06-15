08865d742533b65598f898d94e93396cfd332398
MurielArii <murielarisoaran@gmail.com>
2024-06-15 10:53:21 +0400

    1.68

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -24,7 +24,7 @@ const Service = () => {
                 </g>
             </svg>
             <h1 className="text-5xl first-letter:text-7xl my-10 text-center">Les services diponibles sur le transferts</h1>
-            <div class="grid grid-cols-3 divide-x py-8 mx-20">
+            <div className="grid grid-cols-3 divide-x py-8 mx-20">
                 <div className="flex flex-col justify-center items-center space-y-2">
                     <svg className="w-10 stroke-success" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="12" cy="12" r="10" strokeWidth="2" />
